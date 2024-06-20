45b9f63911c0d77cce6724fc77be330d61ae77ea
Roger <rogersawadogo12@gmail.com>
2024-06-20 13:54:28 +0100

    submission

--- index.js
+++ index.js
@@ -90,10 +90,8 @@ app.post('/api/persons', (req, res) => {
     res.json(newPerson);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+
